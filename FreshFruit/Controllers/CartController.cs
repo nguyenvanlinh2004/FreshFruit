4a9dcffce4500080b9da8cbcb0cd39f6cab48b91
n-van-linh <vanlinhdz2004@gmail.com>
2025-06-11 16:12:24 +0700

    Initial

---
+++ FreshFruit/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreshFruit.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+function App() {
+
+  return (
+    <>
+     <div class="bg-[#5AAE1B] text-white text-sm flex justify-between items-center px-4 sm:px-6 md:px-10 h-9 select-none">
+   <div>
+    Hotline 0918 582 488
+   </div>
+   <div>
+    Thương hiệu trái cây nhập khẩu uy tín chất lượng
+   </div>
+   <div class="flex space-x-4 text-white text-base cursor-pointer">
+    <i aria-hidden="true" class="fab fa-facebook-f" title="Facebook">
+    </i>
+    <i aria-hidden="true" class="fab fa-twitter" title="Twitter">
+    </i>
+    <i aria-hidden="true" class="fab fa-instagram" title="Instagram">
+    </i>
+   </div>
+  </div>
+    </>
+  )
+}
+
+export default App
